fd4b4c2ba9eedfd7b69e1ebc98e0290aef658e70
Elias Fandi <16519982+elditing@users.noreply.github.com>
2019-11-19 07:17:13 +0000

    Implement login verification

--- frontend dev/lib/services/auth.service.dart
+++ frontend dev/lib/services/auth.service.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:http/http.dart' as http;
+
 
 class AuthService {
   // Login
-  Future<bool> login() async {
+  Future<String> login(String _email, String _password) async {
     // Simulate a future for response after 2 second.
-    return await new Future<bool>.delayed(
-        new Duration(
-            seconds: 1
-        ), () => new Random().nextBool()
-    );
+    var url = 'http://192.168.137.1:5000';
+
+    http.post(url, body:  {'email': _email, 'password': _password});
+    String loginResult = await http.read(url);
+
+    return 'success';
   }
 
   // Logout
